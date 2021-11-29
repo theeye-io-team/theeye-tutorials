2afb249a32739c651695383e133107e4a7dbaa9f
facugon <facugon@theeye.io>
2021-11-29 15:16:33 -0300

    TheEye Boilerplate

---
+++ src/lib/theeye_boilerplate.js
@@ -0,0 +1,109 @@
+// error and output handlers must go first.
+
+/**
+ * @param {Object}
+ * @prop {Mixed} data
+ * @prop {Array} components
+ * @prop {Object} next
+ */
+const successOutput = ({ data, components, next }) => {
+  // https://documentation.theeye.io/core-concepts/scripts/#passing-arguments-in-workflow
+  const output = {
+    state: "success",
+    data,
+    components, // https://documentation.theeye.io/core-concepts/tasks/script_type/#components
+    next
+  }
+  console.log( JSON.stringify(output) )
+  process.exit(0)
+}
+
+/**
+ * @param {Error} err
+ */
+const failureOutput = (err) => {
+  console.error(err)
+  const output = {
+    state: "failure",
+    data: {
+      message: err.message,
+      code: err.code,
+      data: err.data 
+    }
+  }
+  console.error( JSON.stringify(output) )
+  process.exit(1)
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(reason, 'Unhandled Rejection at Promise', p)
+  failureOutput(reason)
+})
+
+process.on('uncaughtException', err => {
+  console.error(err, 'Uncaught Exception thrown')
+  failureOutput(err)
+})
+
+class TheEyeBoilerplate {
+  constructor (handler) {
+    if (typeof handler !== 'function') {
+      throw new Error('handler function expected')
+    }
+
+    this.handler = handler
+  }
+
+  async run () {
+    const args = process.argv.slice(2)
+
+    // collect handler result
+    await this.execute(args)
+
+    // generate the lastline
+    this.report()
+
+    return this
+  }
+
+  /*
+   * @param {Array} args
+   * @return {TheEyeBoilerplate}
+   */
+  async execute (args) {
+    try {
+      const value = this.handler(args)
+      let output
+      if (value instanceof Promise) {
+        // await promise resolve
+        output = await value
+      } else {
+        output = value
+      }
+
+      if (
+        output !== undefined &&
+        output !== null &&
+        output.hasOwnProperty('data')
+      ) {
+        this.output = output
+      } else {
+        this.output = { data: output }
+      }
+    } catch (err) {
+      this.err = err
+    }
+
+    return this
+  }
+
+  report () {
+    if (this.err) {
+      failureOutput(this.err)
+    } else {
+      successOutput(this.output)
+    }
+  }
+}
+
+module.exports = TheEyeBoilerplate
