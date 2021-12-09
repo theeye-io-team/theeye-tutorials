689f18d124208212c1e8f5424baa0e1bc034f9db
facugon <facugon@theeye.io>
2021-12-09 11:04:47 -0300

    simplest report and calculation

--- src/totales_report.js
+++ src/totales_report.js
@@ -8,38 +8,48 @@
  *
  */
 const main = module.exports = async (args) => {
-
-  const [ filename, category ] = args
-
-  const fileStream = fs.createReadStream(filename)
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  const matches = []
-
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
-  for await (const line of rl) {
-    // Each line in input.txt will be successively available here as `line`.
-
-    // CODE FOR SECTION 3B.
-    const pattern = new RegExp(category)
-    if (pattern.test(line)) {
-      console.log(`Line for ${category}: ${line}`);
-
-      const extract = new RegExp(' ([0-9]{1}[0-9\.,]*)$')
-      const groups = line.match(extract)
-      matches.push( groups[1] )
-    }
+  let values
+  try {
+    values = JSON.parse(args[0])
+  } catch (err) {
+    throw new Error('values - invalid JSON format')
   }
 
-  return matches
+  const topic = args[1]
+
+  if (!Array.isArray(values)) {
+    throw new Error('values - invalid argument')
+  }
+
+  if (values.length === 0) {
+    return 'No se encontro el concepto'
+  }
+
+  let lines = ['Reporte de totales']
+  lines.push(`---------------------------`)
+  let sum = 0
+  for (let index = 0; index < values.length; index++) {
+    const value = values[index]
+    const num = strToNumCalc(value)
+    sum += num
+    lines.push(value)
+  }
+
+  lines.push(`---------------------------`)
+  lines.push(`Total: ${sum.toLocaleString('es-AR')}`)
+
+  return lines.join("\n")
+}
+
+/**
+ * @param String
+ * @return Number
+ */
+const strToNumCalc = (str) => {
+  return Number(str.replace('.','').replace(',','.'))
 }
 
 if (require.main === module) {
-  main(process.argv.slice(2))
+  main(process.argv.slice(2)).then(console.log).catch(console.error)
 }
 
