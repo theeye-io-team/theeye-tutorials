728ab7b98d8f804396d96a2812fe63ffc9bd748b
facugon <facugon@theeye.io>
2021-12-13 17:22:59 -0300

    standard

--- src/descargaBNA.js
+++ src/descargaBNA.js
@@ -1,173 +1,131 @@
-const successOutput = (data) => {
-    // https://documentation.theeye.io/core-concepts/scripts/#passing-arguments-in-workflow
-    const output = {
-      state: "success",
-      data   
-    }
-    console.log( JSON.stringify(output) )
-    process.exit(0)
-  }
-  
-  /**
-   * @param {Error} err
-   */
-  const failureOutput = (err) => {
-    console.error(err)
-    const output = {
-      state: "failure",
-      data: {
-        message: err.message,
-        code: err.code,
-        data: err.data 
-      }
-    }
-    console.error( JSON.stringify(output) )
-    process.exit(1)
-  }
 
-  process.on('unhandledRejection', (reason, p) => {
-    console.error(reason, 'Unhandled Rejection at Promise', p)
-    failureOutput(reason)
-  })
-  
-  process.on('uncaughtException', err => {
-    console.error(err, 'Uncaught Exception thrown')
-    failureOutput(err)
-  })
-
-//require('config/config')
-const config = { webbot: { URL_BNA : 'https://www.bna.com.ar/Personas'}}
+// require('config/config')
+const config = { webbot: { URL_BNA: 'https://www.bna.com.ar/Personas' } }
 const moment = require('moment')
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const retry = require('async-retry')
 const path = require('path')
-//const { url } = require('inspector')
-const URL_BNA = config.webbot.URL_BNA 
+// const { url } = require('inspector')
+const URL_BNA = config.webbot.URL_BNA
 const HtmlTableToJson = require('html-table-to-json')
 
 const dataOutput = (fecha) => {
-    return new Promise(async function(resolve, reject) {
-        try {
+  return new Promise(async function (resolve, reject) {
+    try {
+      const fechaHistorico = moment(fecha).format('DD/MM/YYYY')
+      console.log(fechaHistorico)
 
-            const fechaHistorico = moment(fecha).format('DD/MM/YYYY')
-            console.log(fechaHistorico)
+      await page.waitForSelector('div.tabSmall')
 
-            await page.waitForSelector('div.tabSmall')
+      const tableBilletes = await page.evaluate(() => {
+        const rowsBilletes = document.querySelector('#billetes > table')
+        return rowsBilletes.outerHTML
+      })
 
-            const tableBilletes = await page.evaluate(() => {
-                const rowsBilletes = document.querySelector("#billetes > table")                
-                return rowsBilletes.outerHTML
-                })
+      const billetes = HtmlTableToJson.parse(tableBilletes)
 
-            const billetes = HtmlTableToJson.parse(tableBilletes)
+      const tableDivisas = await page.evaluate(() => {
+        const rowsDivisas = document.querySelector('#divisas > table')
+        return rowsDivisas.outerHTML
+      })
 
-            const tableDivisas = await page.evaluate(() => {
-                const rowsDivisas = document.querySelector("#divisas > table")                
-                return rowsDivisas.outerHTML
-                })
+      const divisas = HtmlTableToJson.parse(tableDivisas)
 
-            const divisas = HtmlTableToJson.parse(tableDivisas)            
-                      
-            await page.click('#buttonHistoricoBilletes')
-            await page.waitForSelector('#fecha')            
-            await page.$eval('#fecha', (element, fecha) => { 
-                    element.value = fecha
-                },fechaHistorico)
-            
-            await page.evaluate( function(){
-                $('#buscarHistorico').click();
-              })            
-            //await page.waitForTimeout(5000)
+      await page.click('#buttonHistoricoBilletes')
+      await page.waitForSelector('#fecha')
+      await page.$eval('#fecha', (element, fecha) => {
+        element.value = fecha
+      }, fechaHistorico)
 
-            const tableDolar = await page.evaluate(() => {
-                const rowsDolar = document.querySelector("#tablaDolar > table")                
-                return rowsDolar.outerHTML
-                })
-            const tableEuro = await page.evaluate(() => {
-                    const rowsEuro = document.querySelector("#tablaEuro > table")                
-                    return rowsEuro.outerHTML
-                })
-            
-            const dolar = HtmlTableToJson.parse(tableDolar)
-            const euro = HtmlTableToJson.parse(tableEuro)
+      await page.evaluate(function () {
+        $('#buscarHistorico').click()
+      })
+      // await page.waitForTimeout(5000)
 
-            const cotizaciones = {
-                "billetes": billetes.results[0],
-                "divisas": divisas.results[0],
-                "historico": { 
-                    "dolar": dolar.results[0],
-                    "euro": euro.results[0]
-                }
-            }
+      const tableDolar = await page.evaluate(() => {
+        const rowsDolar = document.querySelector('#tablaDolar > table')
+        return rowsDolar.outerHTML
+      })
+      const tableEuro = await page.evaluate(() => {
+        const rowsEuro = document.querySelector('#tablaEuro > table')
+        return rowsEuro.outerHTML
+      })
 
-            browser.close()
-            resolve (cotizaciones)
+      const dolar = HtmlTableToJson.parse(tableDolar)
+      const euro = HtmlTableToJson.parse(tableEuro)
 
-        } catch (err) {
-            console.log(err)
-            browser.close()
-            reject(err)            
+      const cotizaciones = {
+        billetes: billetes.results[0],
+        divisas: divisas.results[0],
+        historico: {
+          dolar: dolar.results[0],
+          euro: euro.results[0]
         }
-    })
-}
+      }
 
-
-const preparePage = async(webUrl) => {
-    browser = await puppeteer.launch({
-        headless: false,
-        //headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-features=site-per-process',
-            '--disable-gpu',
-            '--window-size=1920x1080',
-        ]
-    })
-    viewPort = {
-        width: 1300,
-        height: 900
+      browser.close()
+      resolve(cotizaciones)
+    } catch (err) {
+      console.log(err)
+      browser.close()
+      reject(err)
     }
-
-
-    page = await browser.newPage()
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
-    await page.setViewport(viewPort)
-    await page.setDefaultNavigationTimeout(20000)
-    await page.setDefaultTimeout(20000)
-
-    await page.goto(webUrl, {
-        waitUntil: 'networkidle0'
-    })
-
+  })
 }
 
-const main = module.exports = async (fechaParaHistorico) => {
-    moment.locale('es')
-    
-       
+const preparePage = async (webUrl) => {
+  browser = await puppeteer.launch({
+    headless: false,
+    // headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-features=site-per-process',
+      '--disable-gpu',
+      '--window-size=1920x1080'
+    ]
+  })
+  viewPort = {
+    width: 1300,
+    height: 900
+  }
 
-    await retry(async bail =>  {       
-        //console.log(URL_BNA)
-        await preparePage(URL_BNA)
-    }, {
-        retries: 5,
-        onRetry: async err => {
-            console.log(err)
-            console.log('Retrying...')
-            await page.close()
-            await browser.close()
-        }
-    })
+  page = await browser.newPage()
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36')
+  await page.setViewport(viewPort)
+  await page.setDefaultNavigationTimeout(20000)
+  await page.setDefaultTimeout(20000)
 
-    //const resultados = await processDataRequest(fechaParaHistorico)       
-    //const resultados = await dataOutput(fechaParaHistorico)       
-    return resultados = await dataOutput(fechaParaHistorico).then(results => {return results}).catch(error => {        
-        throw new Error(error)        
-    }) 
-      
-        
+  await page.goto(webUrl, {
+    waitUntil: 'networkidle0'
+  })
+}
+
+const main = module.exports = async (args) => {
+  const fechaParaHistorico = args[0]
+
+  moment.locale('es')
+
+  await retry(async bail => {
+    // console.log(URL_BNA)
+    await preparePage(URL_BNA)
+  }, {
+    retries: 5,
+    onRetry: async err => {
+      console.log(err)
+      console.log('Retrying...')
+      await page.close()
+      await browser.close()
+    }
+  })
+
+  // const resultados = await processDataRequest(fechaParaHistorico)
+  // const resultados = await dataOutput(fechaParaHistorico)
+  return resultados = await dataOutput(fechaParaHistorico).then(results => { return results }).catch(error => {
+    throw new Error(error)
+  })
 }
 
 if (require.main === module) {
-  main(process.argv[2], process.argv[3]).then(successOutput).catch(failureOutput)}
+  main(process.argv.slice(2)).then(console.log).catch(console.error)
+}
