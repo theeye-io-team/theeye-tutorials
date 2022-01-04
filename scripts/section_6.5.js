40b1dd9a14afb641348b2f3de52d088a1eb16449
facugon <facugon@theeye.io>
2022-01-04 00:24:18 -0300

    sample request added

--- scripts/section_6.5.js
+++ scripts/section_6.5.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 
 const fs = require('fs')
-const Workflow = require('theeye-sdk/core/api/workflow')
+const https = require('https')
+const { URL } = require('url')
 
-const WF_ID = process.env.WF_ID
-const WF_SECRET = process.env.WF_SECRET
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+const WF_ID = process.env.CASO1_WF_ID
+const WF_SECRET = process.env.CASO1_WF_SECRET
+const API_URL = JSON.parse(process.env.THEEYE_API_URL)
+const ORG_NAME = JSON.parse(process.env.THEEYE_ORGANIZATION_NAME)
 
 // distpacher
 const main = module.exports = async () => {
@@ -22,17 +25,63 @@ const main = module.exports = async () => {
     const mime = 'text/plain'
     const data = `data:${mime};base64,${content.toString('base64')}`
 
-    const task_arguments = [ data, 'BRUTO' ]
-    
     console.log(`running workflow for file ${directory}/${filename}`)
-    
-    const api = new Workflow({ access_token: ACCESS_TOKEN })
-    const workflow = await api.run({ id: WF_ID, secret: WF_SECRET, task_arguments })
-    console.log(workflow.id)
+
+    const task_arguments = [ data, 'BRUTO' ]
+    const response = await launchWorkflow(task_arguments)
+    console.log(response.body)
   }
   
   return {}
 }
 
+const launchWorkflow = (task_arguments) => {
+  return new Promise((resolve, reject) => {
+
+    const wfUrl = `${API_URL}/workflows/${WF_ID}/secret/${WF_SECRET}/job?customer=${ORG_NAME}`
+    const url = new URL(wfUrl)
+
+    const reqOpts = {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      port: url.port,
+      hostname: url.hostname,
+      path: `${url.pathname}${url.search}`
+    }
+    
+    const req = https.request(reqOpts, res => {
+      let str = ''
+      res.on('data', d => {
+        if (d) { str += d; }
+      })
+      res.on('end', () => {
+        res.body = str
+        
+        if (res.statusCode >= 500) {
+          const error = new Error('Internal Server Error')
+          error.res = res
+          return reject(error)
+        }
+        if (res.statusCode >= 400) {
+          const error = new Error('Invalid Request')
+          error.res = res
+          return reject(error)
+        }
+        resolve(res)
+      })
+    })
+
+    req.on('error', reject)
+
+    if (task_arguments) {
+      console.log(task_arguments)
+      const payload = JSON.stringify(task_arguments)
+      req.write(payload)
+    }
+
+    req.end()
+  })
+}
+
 // invoke main and capture result output
-main().then(successOutput).catch(failureOutput)
+main().then(console.log).catch(console.error)
