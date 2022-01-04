5f3cd3d905a227024a8112046551379311bdf175
facugon <facugon@theeye.io>
2022-01-04 14:30:29 -0300

    script 6.2 roll back

--- scripts/section_6.2.2.js
+++ scripts/section_6.2.2.js
@@ -4,17 +4,15 @@ const fs = require('fs')
 
 const main = module.exports = async () => {
   const args = process.argv.slice(2)
-
   const JOB_ID = JSON.parse(process.env.THEEYE_JOB).id
-
   const filename = args[0]
 
   const zip = new AdmZip(filename)
-  const targetName = `${process.env.EXTRACTION_PATH}/`
+  const targetName = `${process.env.EXTRACTION_PATH}/${JOB_ID}`
   zip.extractAllTo(targetName, true)
 
-  const files = fs.readdirSync(process.env.EXTRACTION_PATH)
-  return { data: files }
+  const files = fs.readdirSync(targetName)
+  return { data: [ files.map(filename => `${targetName}/${filename}`) ] }
 }
 
 if (require.main === module) {
